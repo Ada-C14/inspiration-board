f272e6e7f1ce1f9c60286fe253e904ebb54ba02f
Emily Nagai <emnagai89@gmail.com>
2021-01-13 13:08:46 -0800

    NewCardForm setup

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,6 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+const NewCardForm = () => {
+    const [formFields, setFormFields] = useState({
+        text: '',
+        emoji: '',
+    })
+
+    return (
+        <form className='new-card-form'>
+            <div>
+                <label htmlFor='text'>text:</label>
+                <input text='text' />
+            </div>
+            <div>
+                <label htmlFor='emoji'>emoji:</label>
+                <input emoji='emoji'/>
+            </div>
+            <input
+                type='submit'
+                value='Add Card'
+            />
+        </form>
+    )
+}
+
+export default NewCardForm;
