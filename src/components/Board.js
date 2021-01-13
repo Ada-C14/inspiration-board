fcb32c52a71cfc08c58a16e13bc7d6f54872a8e6
gessica <gessicamohr18@gmail.com>
2021-01-13 15:00:11 -0800

    implemented card component, and board component is displaying all the cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,9 +8,14 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = () => {
+  const cardComponents = CARD_DATA.cards.map((card) => {
+    return (
+      <Card text={card.text} emoji={card.emoji} />
+    )
+  })
   return (
     <div>
-      Board
+      {cardComponents}
     </div>
   )
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,22 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
+
+
   return (
     <div className="card">
-      Card
+      <div className="card__content">
+        <p className="card__content-text">{props.text}</p>
+        <p className="card__content-emoji">{props.emoji ? emoji.getUnicode(props.emoji) : ''}</p>
+      </div>
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string.isRequired
 };
 
 export default Card;
