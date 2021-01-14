03e39d573212cba222416d56a00350a03cac8283
Li Dai <li.lea.dai@gmail.com>
2021-01-13 22:05:30 -0800

    api call to get all cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,7 +7,28 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+
+  const [cards, setCards] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+
+  useEffect(() => {
+    axios.get(`${props.url}${props.boardName}/cards`)
+      .then((response) => {
+        const apiCards = response.data;
+        setCards(apiCards);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  }, []);
+  
+  const cardComponents = cards.map(({card}) => {
+    return(
+    <Card key={card.id} card={card}/>
+    );
+  });
   return (
     <div>
       Board
