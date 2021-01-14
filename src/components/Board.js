e5ff9086de8a3e2be5e8dc4035ff0d2db1f943b5
marks214 <aimee.oz.x@gmail.com>
2021-01-14 15:29:57 -0800

    refactor

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -8,28 +8,42 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = (props) => {
-  const cards = CARD_DATA.cards.map ((card) => {
-    return (
-      <Card
-      key={card.id}
-      text={card.text}
-      emoji={card.emoji}
-      />
-    );
-  })
+  const API_URL_BASE = `${props.url}/${props.boardName}`
+  const [cardData, setCardData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  // const cardData = CARD_DATA.cards.map ((card) => {
+  //   return (
+  //     <Card
+  //     key={card.id}
+  //     text={card.text}
+  //     emoji={card.emoji}
+  //     />
+  //   );
+  // })
+  
 
   useEffect(() => {
-    axios.get(props.url/props.boardName)
+    axios.get(API_URL_BASE)
     .then((response) => {
-      console.log(response.data);
-    })
+      // Get the list of card-data
+      const apiCardData = response.data.map((cardData) => {
+        return {
+          id: cardData.card.id,
+          emoji: cardData.card.emoji,
+          text: cardData.card.text}
+      });
+      // Set the state
+      setCardData(apiCardData);
+      })
     .catch((error) => {
-      console.log(error.data)
+      setErrorMessage(error.message);
+      console.log(error.message);
     });
-  }, []);
+  }, [API_URL_BASE]);
   return (
     <div className="board">
-      {cards}
+      {errorMessage ? <div><h2 className="error-msg">{errorMessage}</h2></div> : '' }
+      {/* {cardData} */}
     </div>
   )
 };
