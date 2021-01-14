1314a179b319f60afbc61f50f0db8860813e63ad
roshni-patel <rpatel01@wesleyan.edu>
2021-01-13 21:56:49 -0600

    Working on wave 2

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,7 +7,22 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const [cardList, setCardList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${props.url}${props.boardName}/cards`) 
+      .then((response) => {
+        // console.log(response)
+        const apiCardList = response.data; 
+        setCardList(apiCardList);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  }, []);
+
   const cardComponents = CARD_DATA.cards.map((card) => {
     return (
     <Card text={card.text} emoji={card.emoji}/>
