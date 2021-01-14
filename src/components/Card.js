58ff58a31e3f696846d2b56add36ebfebe51e64f
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-01-13 17:19:23 -0800

    added emoji library, moved files onto .gitignore

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,19 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      Card
+      <p>Inspirational Message:{props.text}</p>
+      <p> Emoji:{props.emoji} </p>
     </div>
   )
 }
 
 Card.propTypes = {
-
+  id: PropTypes.number,
+  text: PropTypes.string,
+  emoji: PropTypes.string,
 };
 
 export default Card;
