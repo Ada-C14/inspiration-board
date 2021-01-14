dadf6435edb5b266b5e12ecf3e7321b8d13d9395
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-01-13 20:12:58 -0800

    updated the card-data.json to move to e

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,14 +7,31 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const API_URL_BASE = "https://inspiration-board.herokuapp.com/";
+const API_BOARD_URL = "https://inspiration-board.herokuapp.com/";
 
-const Board = () => {
+const Board = (props) => {
+
+  const [cards, setCards] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get(API_BOARD_URL)
+      .then((response) => {
+        const apiCards = response.data
+        setCards(apiCards);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  }, []);
+
+  
   const cardComponents = CARD_DATA.cards.map((card) => {
     return (
       <Card text ={card.text} emoji={card.emoji} />
-    )
-  })
+    );
+  });
+
   return (
     <div>
       {cardComponents}
