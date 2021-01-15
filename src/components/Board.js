685016378828eb645ee6a95ea4571be7e8a4ea07
marks214 <aimee.oz.x@gmail.com>
2021-01-14 18:21:52 -0800

    able to get the card data-returns blank board, but no errors

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -6,18 +6,35 @@ import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
+  
+const cards = CARD_DATA.cards.map ((card) => {
+  return (
+    <Card
+    key={card.id}
+    text={card.text}
+    emoji={card.emoji}
+    />
+  );
+})
 
+const Board = (props) => {
+  const API_URL_BASE = `${props.url}/${props.boardName}/cards`
+  const [cards, setCards] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-const Board = () => {
-  const cards = CARD_DATA.cards.map ((card) => {
-    return (
-      <Card
-      key={card.id}
-      text={card.text}
-      emoji={card.emoji}
-      />
-    );
-  })
+  useEffect( () => {
+    axios.get(API_URL_BASE)
+    .then((response) => {
+      // need to return card data, need to create list
+      const apiCardData = response.data;
+      console.log(apiCardData)
+      setCards(apiCardData.card)
+    })
+    .catch((error) => {
+      // Still need to handle errors
+      setErrorMessage(error.message);
+    });
+  }, []);
 
   return (
     <div className="board">
