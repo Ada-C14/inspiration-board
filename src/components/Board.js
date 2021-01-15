2ddffe30ad871625cb0c45766fbf7168b1f5b98b
Yingjing Lin <yjlin789@gmail.com>
2021-01-15 00:06:13 -0800

    use get to display all the cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const [cards, setCards] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    axios.get(props.url)
+    .then((response) => {
+      const allCards = response.data
+      setCards(allCards)
+    })
+    .catch((error) => {setErrorMessage(error.message);});
+  }, []);
+
+
+  
+
+
+  const displayCards = cards.map((card) => {
+    return (
+      <Card key={card.card.id} card={card.card} />
+    )
+  });
+
   return (
     <div>
-      Board
+      {displayCards}
     </div>
   )
+
+  
 };
 Board.propTypes = {
-
+  
 };
 
 export default Board;
