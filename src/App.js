02d95ffeadf1e594cc13bdcfc2f6d5ae20f369c3
Emily Nagai <emnagai89@gmail.com>
2021-01-14 17:24:44 -0800

    added cardUrl for delete path

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => {
       <header className="header">
         <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
       </header>
-      {/* {errorMessage ? <div className='error-msg'>{errorMessage}</div> : ''} */}
       <Board
         url="https://inspiration-board.herokuapp.com/boards/"
+        cardUrl="https://inspiration-board.herokuapp.com/cards/"
         boardName={`emirry`}
       />
     </section>
