1b04129e2dad24285d0b7154721ae3643d88a18c
MonaRahmani <monarahmanimehr@gmail.com>
2021-01-14 17:26:57 -0800

    wave1-Bord Component renders a list of cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,10 +8,23 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = () => {
+
+  // render a list of cards
+  // CARD_DATA is a obj that has a key(cards) and value(array)
+  const cardComponents = CARD_DATA.cards.map(card => {
+    return (
+      <Card
+        text={card.text}
+        emoji={card.emoji}
+      />
+    )
+  })
+
+
+  
   return (
     <div>
-      Board
-      <Card/>
+      {cardComponents}
     </div>
   )
 };
