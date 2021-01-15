abec2729124cc674eb7981b672a6e5059a833047
marks214 <aimee.oz.x@gmail.com>
2021-01-14 17:02:44 -0800

    went back a few steps, need to work on having a form for user to fill out first, then work on what to do w/ their data/submission

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,43 +7,21 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = (props) => {
-  const API_URL_BASE = `${props.url}/${props.boardName}`
-  const [cardData, setCardData] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
-  // const cardData = CARD_DATA.cards.map ((card) => {
-  //   return (
-  //     <Card
-  //     key={card.id}
-  //     text={card.text}
-  //     emoji={card.emoji}
-  //     />
-  //   );
-  // })
-  
 
-  useEffect(() => {
-    axios.get(API_URL_BASE)
-    .then((response) => {
-      // Get the list of card-data
-      const apiCardData = response.data.map((cardData) => {
-        return {
-          id: cardData.card.id,
-          emoji: cardData.card.emoji,
-          text: cardData.card.text}
-      });
-      // Set the state
-      setCardData(apiCardData);
-      })
-    .catch((error) => {
-      setErrorMessage(error.message);
-      console.log(error.message);
-    });
-  }, [API_URL_BASE]);
+const Board = () => {
+  const cards = CARD_DATA.cards.map ((card) => {
+    return (
+      <Card
+      key={card.id}
+      text={card.text}
+      emoji={card.emoji}
+      />
+    );
+  })
+
   return (
     <div className="board">
-      {errorMessage ? <div><h2 className="error-msg">{errorMessage}</h2></div> : '' }
-      {/* {cardData} */}
+      {cards}
     </div>
   )
 };
