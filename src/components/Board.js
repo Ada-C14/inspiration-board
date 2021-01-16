d60418c453dfd5ba483041fcbe8976d3b2ad4a38
beauttie <bak02013@pomona.edu>
2021-01-16 15:26:25 -0800

    Build Card component and render list of cards on Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,15 +7,20 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const cardList = CARD_DATA.cards.map((card, i) => {
+  return <Card key={i} text={card.text} emoji={card.emoji} />
+});
+
+const Board = (props) => {
   return (
-    <div>
-      Board
+    <div className="board">
+      {cardList}
     </div>
   )
 };
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired
 };
 
 export default Board;
