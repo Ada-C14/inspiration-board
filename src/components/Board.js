3884a08720786b4efca88fa906077f0b8fa02763
Ren Carothers <rencarothers@gmail.com>
2021-01-16 20:14:45 -0800

    begins writing delete and add functions, form...not working yet

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,10 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 // const API_URL_BASE = 'https://inspiration-board.herokuapp.com/boards/oatcake/cards';
+const CARD_URL_BASE = `https://inspiration-board.herokuapp.com/cards/`
 
 const Board = (props) => {
 
@@ -41,6 +42,7 @@ const Board = (props) => {
           id={card[`card`][`id`]}
           text={card[`card`][`text`]}
           emoji={card[`card`][`emoji`]}
+          deleteCard={deleteCard}
       />
     )
   }
@@ -48,9 +50,44 @@ const Board = (props) => {
     return cardsComponentArray;
   }; 
 
+  const addCard = (card) => {
+    axios.post(`${props.url}${props.boardName}/cards`, card)
+      .then((response) => {
+        const updatedCards = [...cardList, response.data]
+        console.log("Card successfully added.");
+        setCardList(updatedCards);
+        setErrorMessage('');
+      })
+      .catch((error) => {
+        setErrorMessage('Card could not be added');
+        console.log(errorMessage);
+      });
+  }
+
+  const deleteCard = (id) => {
+    console.log(id);
+    const result = cardList.filter(card => card.card.id === id);
+    console.log(result);
+    if (result) {
+      axios.delete(`${CARD_URL_BASE}${id}`)
+      .then((response) => {
+        // What should we do when we know the post request worked?
+        const updatedData = cardList.filter(card => card != result);
+        setCardList(updatedData);
+        setErrorMessage('');
+        console.log(`Card ${id} successfully deleted`);
+      })
+      .catch((error) => {
+        // What should we do when we know the post request failed?
+        setErrorMessage(error.message);
+      });
+    }
+  };
+
   return (
     <div className = 'board'>
       { errorMessage ? <div><h2 className="validation-errors-display">{errorMessage}</h2></div> : generateCards(cardList) }
+      <NewCardForm onAddCard={addCard} />
     </div>
   )
 };
--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,3 +4,50 @@ import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+const NewCardForm = () => {
+
+    const generateEmojiList = () => {
+        let emojiArray = [];
+        
+        for (const emojiOption of EMOJI_LIST)
+            if (emojiOption === "") {
+                emojiArray.push(<option>No Emoji</option>)
+            }
+            else {
+                emojiArray.push(<option>{emoji.getUnicode(`${emojiOption}`)}</option>)
+            }; 
+        return emojiArray;
+    }; 
+
+    return (
+        <div className="new-card-form">
+        <h3 className="new-card-form__header">New Card Form</h3>
+
+        <form className="new-card-form__form" onSubmit="">
+
+            <div className="new-card-form__form-label">
+                <input
+                    // name={field.key}
+                    placeholder='Inspirational text...'
+                    value=''
+                    // value={formFields[field.key] || ''}
+                    // onChange={onInputChange}
+                    className='new-card-form__form-textarea'
+                    type="text-area" 
+                />
+                <select className="new-card-form__form-select">
+                    <option selected="">Select Emoji...</option>
+                    {generateEmojiList()}
+                </select>
+            </div>
+
+            <div className="new-card-form__form-button">
+            <input type="submit" value="Add Card" className="new-card-form__form-button" />
+            </div>
+        </form>
+        </div>
+    );
+}
+
+export default NewCardForm;
