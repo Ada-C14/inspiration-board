399416a8db0f9c85cc12dabda9d480f1490d1ff1
Ren Carothers <rencarothers@gmail.com>
2021-01-16 17:58:26 -0800

    begins wave 2, uses API get request to generate cards from project board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,28 +7,48 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+const API_URL_BASE = 'https://inspiration-board.herokuapp.com/boards/oatcake/cards';
+
 const Board = () => {
 
-  const generateCards = () => {
-    const cards = CARD_DATA[`cards`];
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL_BASE)
+      .then((response) => {
+        // Get the list of students
+        const apiCardList = response.data;
+        // Set the state
+        setCardList(apiCardList);
+      })
+      .catch((error) => {
+        // Still need to handle errors
+      });
+  }, []);
+  // ...
+
+  const generateCards = (cards) => {
     let cardsComponentArray = [];
+    // console.log(cards)
 
   for (const card of cards) 
+  // console.log(card[`card`][`id`])
   {
     cardsComponentArray.push(
       <Card
-          text={card[`text`]}
-          emoji={card[`emoji`]}
+          id={card[`card`][`id`]}
+          text={card[`card`][`text`]}
+          emoji={card[`card`][`emoji`]}
       />
     )
   }
 
     return cardsComponentArray;
-  }  
+  }; 
 
   return (
     <div className = 'board'>
-      {generateCards()}
+      {generateCards(cardList)}
     </div>
   )
 };
