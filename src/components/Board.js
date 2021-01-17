cc10e3eeb94fbd750001c9ab647c1a94c8311991
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-17 15:59:49 -0800

    render cards in Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,21 +8,22 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const generateCardComponents = () => {
-  const cards = CARD_DATA.cards
+  const cards = CARD_DATA.cards;
 
   return cards.map((card, index) => {
     return (
       <Card key={ index } card={ card } />
     )
-  })
+  });
 }
 
 const Board = () => {
+  const cards = generateCardComponents();
 
   return (
     <div>
       Board
-      
+      { cards }
     </div>
   )
 };
