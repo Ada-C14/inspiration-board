f0984a5cc5345d094973d1a10e0d0854ce11badb
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-01-17 12:49:48 -0500

    added axios

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -9,10 +9,62 @@ import CARD_DATA from '../data/card-data.json';
 
 const Board = (props) => {
 
-  const boardComponents = CARD_DATA.cards.map((card, key) => {
+  const [cardList, setCardList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get(props.url + props.boardName + '/cards')
+      .then((response) => {
+        // Get the list of cards
+        const apiCardList = response.data;
+        // Set the state
+        setCardList(apiCardList);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message)
+      });
+  }, []);
+
+  // const updateCard = (updatedCard) => {
+  //   const cards = [];
+
+  //   cardList.forEach((card) => {
+  //     if (card.id === updatedCard.id) {
+  //       cards.push(updatedCard);
+  //     } else {
+  //       cards.push(card);
+  //     }
+  //   });
+
+  //   setCardList(cards);
+  // }
+
+  const addCard = (card) => {
+    axios.post(props.url + props.boardName, card)
+      .then((response) => {
+        // What should we do when we know the post request worked?
+        const updatedData = [...cardList, response.data];
+        setCardList(updatedData);
+        setErrorMessage('');
+      })
+      .catch((error) => {
+        // What should we do when we know the post request failed?
+        setErrorMessage(error.message);
+      });
+  }
+
+  // return (
+  //   <div className="board">
+  //     {errorMessage ? <div><h2 className="error-msg">{errorMessage}</h2></div> : ''}
+  //     {/* <CardCollection cards={cardList} onUpdateCard={updateCard} /> */}
+  //     <NewCardForm addCardCallback={addCard} />
+  //   </div>
+
+  const boardComponents = cardList.map(({card}) => {
+    console.log(card)
     return (
-      <div className="board" key={key}>
-        <Card
+      <div className="board">
+        <Card key={card.id}
           text={card.text}
           emoji={card.emoji}
         />
@@ -22,6 +74,7 @@ const Board = (props) => {
 console.log(boardComponents)
   return (
     <div>
+      <NewCardForm onAddCallBack={addCard}/>
       {boardComponents}
     </div>
   )
--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,6 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+const NewCardForm = (props) => {
+  const [formFields, setFormFields] = useState({
+    text: '',
+    emoji: '',
+  });
+
+  // event handlers
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields,
+    }
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    props.addCardCallback(formFields);
+
+    setFormFields({
+      text: '',
+      emoji: '',
+    });
+  };
+
+  // validate emoji
+  const emojiValid = () => {
+    return EMOJI_LIST.includes(formFields.emoji)
+    // .match(/\S+@\S+/) || formFields.emoji === '';
+  }
+
+  return (
+    <form
+      className="new-card-form"
+      onSubmit={onFormSubmit}
+      data-testid="NewCardForm--form"
+    >
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input
+          id="text"
+          name="text"
+          onChange={onInputChange}
+          value={formFields.text}
+          className="text"
+        />
+      </div>
+      <div>
+        <label htmlFor="emoji">Emoji:</label>
+        <input
+          id="emoji"
+          name="emoji"
+          onChange={onInputChange}
+          value={formFields.emoji}
+          className={emojiValid() ? "valid" : "invalid"}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Add Card"
+      />
+    </form>
+  );
+}
+
+export default NewCardForm;
