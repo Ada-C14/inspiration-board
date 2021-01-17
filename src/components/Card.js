4411727f18542a5d70a3a1a5bc538152cdae9b3a
Jasmine <jasylop@gmail.com>
2021-01-17 15:12:30 -0800

    added text and emojis via props to card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
-
+// example: emoji.getUnicode("heart_eyes")
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      Card
+      <div className='card__content'>
+        <div className='card__content-text'>{props.message.text}</div>
+        <div className='card__content-emoji'>{props.message.emoji && emoji.getUnicode(props.message.emoji)}</div>
+      </div>
     </div>
   )
 }
+
+
+
 
 Card.propTypes = {
 
