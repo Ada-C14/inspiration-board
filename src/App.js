b42beec892f377f06d2c083943f360a2514ba2db
Jasmine <jasylop@gmail.com>
2021-01-17 20:47:18 -0800

    added url for cards

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const App = () => {
         <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
       </header>
       <Board
-        url="https://inspiration-board.herokuapp.com/boards/"
+        boardUrl="https://inspiration-board.herokuapp.com/boards/"
+        cardUrl="https://inspiration-board.herokuapp.com/cards/"
         boardName={`jasmine-y-lopez`}
       />
     </section>
