6fe5e39f702bf9427c35abc9d2627cdde68ad053
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-17 16:55:55 -0800

    add useEffect get request

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
 
-const generateCardComponents = () => {
-  const cards = CARD_DATA.cards;
-
-  return cards.map((card, index) => {
+const generateCardComponents = (cards) => {
+  return cards.map(card => {
     return (
-      <Card key={ index } card={ card } />
+      <Card key={ card.id } card={ card } />
     )
   });
 }
 
-const Board = () => {
-  const cards = generateCardComponents();
+const Board = ({ boardName, url }) => {
+  const [cards, setCards] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  useEffect(() => {
+    axios.get(`${url}${boardName}/cards`)
+      .then((response) => {
+        const responseCards = response.data.map(({ card }) => card);
+        setCards(responseCards);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        console.error(error.message);
+      });
+  }, []);
+
+  const cardComponents = generateCardComponents(cards);
 
   return (
     <div>
-      { cards }
+      { errorMessage || cardComponents }
     </div>
   )
 };
 Board.propTypes = {
-
+  boardName: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
 export default Board;
