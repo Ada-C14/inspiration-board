05e6a4ee61064b1cc8ce1741600157b5e24c02f4
Jasmine <jasylop@gmail.com>
2021-01-17 20:48:26 -0800

    added a deletecard and addcard method that makes request to api and updates state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -6,17 +6,77 @@ import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
+import NewCardFrom from './NewCardForm';
 
-const Board = () => {
+
+const Board = (props) => {
+
+  const [cardList, setCardList] = useState([]);
+
+  // populates board with cards when component first mounts
+  useEffect(() => {
+    axios.get(props.boardUrl + props.boardName + '/cards')
+      .then( (response) => {
+        //successful response
+        console.log(response.data);
+        setCardList(response.data);
+      })
+      .catch((error) => {
+        // console.log(error)
+      });
+  }, []);
+
+
+  const deleteCard = (id) => {
+    axios.delete(props.cardUrl + id)
+      .then( () => {
+        axios.get(props.boardUrl + props.boardName + '/cards')
+          .then( (response) => {
+            //successful response
+            setCardList(response.data);
+          })
+          .catch((error) => {
+            // console.log(error)
+          });
+      })
+      .catch( (error) => {
+        // console.log(error);
+      })
+  }
+
+  const addCard = (card) => {
+    console.log(card)
+    console.log(props.boardUrl + props.boardName + '/cards')
+    axios.post(props.boardUrl + props.boardName + '/cards', {...card})
+    
+      .then( (response) => {
+        axios.get(props.boardUrl + props.boardName + '/cards')
+        .then( (response) => {
+          //successful response
+          setCardList(response.data);
+        })
+        .catch((error) => {
+          // console.log(error)
+        });
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   // using and incrementer in map not the best way to add a key??
-  const cardComponents = CARD_DATA.cards.map ((card, i) => {
+  const cardComponents = cardList.map ((element) => {
     return (
-      <Card message={card} key={i}/>
+      <Card card={element.card} key={element.card.id} deleteCallback={deleteCard} />
     )
   })
   return (
     <div>
-      {cardComponents}
+      <NewCardFrom addCardCallback={addCard} />
+      <div className="board">
+        {cardComponents}
+      </div>
     </div>
   )
 };
