73667ea921acdb2b2f5d31557612cbf8d468b333
India Kato <india.kato@gmail.com>
2021-01-18 13:45:25 -0800

    api get call

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,7 +7,22 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const [cardList, setCardList] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    axios.get(`${props.url}${props.boardName}/cards`)
+      .then((response) => {
+        const apiCardList = response.data;
+        setCardList(apiCardList);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        console.log(error.message);
+      });
+  }, []);
+
   return (
     <div>
       Board
@@ -15,7 +30,8 @@ const Board = () => {
   )
 };
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired
 };
 
 export default Board;
