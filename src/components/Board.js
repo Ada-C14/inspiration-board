3a3b9fcf87d3cd2786b86d88395730c457a4aa28
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2021-01-17 17:46:27 -0800

    Wave 2 - add API functionality, done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
-  const boardComponents = CARD_DATA.cards.map((card, i) => {
+const Board = (props) => {
+  // const boardComponents = CARD_DATA.cards.map((card, i) => {
+  //   return (
+  //     <li key={i}>
+  //       <Card 
+  //         text={card.text} 
+  //         emoji={card.emoji} 
+  //       />
+  //     </li>
+  //   );
+  // });
+
+  const [cards, setCards] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    axios.get(props.url)
+    .then((response) => {
+      const allCards = response.data
+      setCards(allCards)
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+    });
+  }, []);
+
+  const cardList = cards.map((card) => {
     return (
-      <li key={i}>
-        <Card 
-          text={card.text} 
-          emoji={card.emoji} 
-        />
-      </li>
-    );
+      <Card key={card.card.id} card={card.card} />
+    )
   });
 
   return (
     <div>
-      { boardComponents }
+      { cardList }
     </div>
   )
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,14 +7,20 @@ import './Card.css';
 const Card = (props) => {
   return (
     <div className="card">
-      {props.text}
-      {emoji.getUnicode(`${props.emoji}`)}
+      {props.card.text}
+      {props.card.emoji ? emoji.getUnicode(props.card.emoji) : ''}
     </div>
   )
 }
 
 Card.propTypes = {
-
+  card: PropTypes.objectOf(PropTypes.shape(
+    {
+      id: PropTypes.number,
+      text: PropTypes.string,
+      emoji: PropTypes.string,
+    }
+  ))
 };
 
 export default Card;
