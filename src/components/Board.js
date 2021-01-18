4219f7e09db3a8e7b92836e5f7bf106da71d965b
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2021-01-17 16:18:18 -0800

    Wave 1 - load board w cards (text&emoji), complete

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,9 +8,20 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = () => {
+  const boardComponents = CARD_DATA.cards.map((card, i) => {
+    return (
+      <li key={i}>
+        <Card 
+          text={card.text} 
+          emoji={card.emoji} 
+        />
+      </li>
+    );
+  });
+
   return (
     <div>
-      Board
+      { boardComponents }
     </div>
   )
 };
