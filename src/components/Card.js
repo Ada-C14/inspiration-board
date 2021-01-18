2ae186459299671bdf6f254aa392f21d96e1b559
Alice Boone <alicegbonel@gmail.com>
2021-01-18 12:51:03 -0800

    added propTypes and a  button to delete card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,19 +7,21 @@ import './Card.css';
 const Card = (props) => {
   return (
     <div className="card">
-      <div className="card__content">
-        <p className="card__content-text">{ props.text ? props.text : null}</p>
-        <p className="card__content-emoji">{props.emoji ? emoji.getUnicode(props.emoji): null}</p>
+    <div className="card__content">
+      <p className="card__content-text">{ props.text ? props.text : null}</p>
+      <p className="card__content-emoji">{props.emoji ? emoji.getUnicode(props.emoji): null}</p>
+    </div>
+    <div>      
+        <button className='card_delete' onClick={() => props.deleteCardCallBack(props.id)}>x</button>
       </div>
-    </div>	  
-  )
+  </div>	  
+)
 }
 
 Card.propTypes = {
   text: PropTypes.string,
   emoji: PropTypes.string,
-  id: PropTypes.number
+  id: PropTypes.number.isRequired,
+  deleteCardCallBack: PropTypes.func.isRequired,
 };
-
 export default Card;
-ç
