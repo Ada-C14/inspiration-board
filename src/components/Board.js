ff819a60c9718dbf47fb3ff66fab83197c1d2b59
Mackenzie <mvlofthus@gmail.com>
2021-01-18 23:07:50 -0800

    cards show hardcoded data

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,14 +7,48 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+const dummyCards = [
+  {
+      "card": {
+          "id": 2717,
+          "text": "100",
+          "emoji": '100'
+      }
+  },
+  {
+      "card": {
+          "id": 2718,
+          "text": "BE EXCELLENT TO EACHOTHER",
+          "emoji": null
+      }
+  },
+  {
+      "card": {
+          "id": 2719,
+          "text": "BREATHE",
+          "emoji": null
+      }
+  }]
+
+  const generateBoard = (dummyCards) => {
+    const cards = dummyCards.map((card) => {
+      return (
+        <Card id={card.card.id} text={card.card.text} emoji={card.card.emoji}/>
+      );
+    });
+    return cards;
+  }
+
 const Board = () => {
+  const cardList = generateBoard(dummyCards)
   return (
-    <div>
-      Board
+    <div className="board">
+      {cardList}
     </div>
   )
 };
 Board.propTypes = {
+  
 
 };
 
