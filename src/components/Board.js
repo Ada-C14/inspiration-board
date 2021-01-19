9f56bc38a6870b892c43b21c7516650de5ad9bff
Rachael Gomez <gomezrc1220@gmail.com>
2021-01-18 17:00:48 -0800

    maybe made the callback function for deleting?

--- src/components/Board.js
+++ src/components/Board.js
@@ -17,7 +17,7 @@ const Board = (url, boardName) => {
   useEffect(() => {
     axios.get(API_URL_BASE)
       .then((response) => {
-        const apiCards = response.data;
+        let apiCards = response.data;
         if (apiCards.length === 0) {
           setCards(CARD_DATA['cards'])
           console.log(CARD_DATA['cards'])
@@ -30,6 +30,26 @@ const Board = (url, boardName) => {
         console.log(error.message);
       });
   }, []);
+
+  const deleteCard = (cardId) => {
+    let cardsLength = cards.length
+    for (let i = 0; i < cardsLength; i ++) {
+      if (cardId === cards[i].id) {
+        axios.delete(`${url}/cards/${cardId}`)
+      .then((response) => {
+        let newCards = response.data;
+        console.log(`Card ${cardId} was deleted.`)
+        setCards(newCards);
+      })
+      .catch((error) => {
+        const message = `${cardId} was not deleted. ${error.message}`
+        setErrorMessage(message);
+        console.log(message)
+      })
+    
+      }
+    }
+};
   
   const makeCards = CARD_DATA.cards.map((card,i) => {
     return(
