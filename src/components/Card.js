eb2128517de2b0df884198aa229db683fa6de2b0
Stacy <lundquist.stacy@gmail.com>
2021-01-18 16:53:18 -0800

    dropdown update menus - wip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
 const Card = (props) => {
+  const [updateFields, setUpdateFields] = useState({
+    text: '',
+    emoji: '',
+  });
 
-const showEmoji = (emojiString) => {
-  return emoji.getUnicode(emojiString)
-}
+  const showEmoji = (emojiString) => {
+    return emoji.getUnicode(emojiString)
+  };
 
+  const onUpdateFieldChange = (event) => {
+    const newUpdateFields = {...updateFields,};
+    newUpdateFields[event.target.name] = event.target.value;
+    setUpdateFields(newUpdateFields);
+  };
 
+  const onUpdateSubmit = (event) => {
+
+  };
+// onClick = {
+//   () => props.onUpdateCardCallback(props.id)
+// }
+  function myFunction() {
+    document.getElementById("messageDropdown").classList.toggle("show");
+  };
 
   return (
     <div className="card">
@@ -21,10 +39,28 @@ const showEmoji = (emojiString) => {
         <div className="card__content-emoji">
           {showEmoji(props.emoji)}
         </div>
-        <button onClick={() => props.onDeleteCardCallback(props.id)}
-        className="card__delete">Remove</button>
-        <button onClick={() => props.onUpdateCardCallback(props.id)}
-        className='card__delete'>Update </button>
+        <div><button onClick={() => props.onDeleteCardCallback(props.id)}
+        className="card__delete">Remove</button></div>
+        <div className='dropdown'>
+          <button onClick='myFunction()' className='card__delete'>Update Message</button>
+          <div id='messageDropdown' class='dropdown__content'>
+            <label>Change Message</label>
+            <input id='text' name='text'
+                onChange={onUpdateSubmit}
+                value={updateFields.text}/>
+          </div>
+          <button onClick='myFunction()' className='card__delete'>Update Emoji</button>
+        </div>
+        <form onSubmit={onUpdateSubmit}>
+          <label>Change Message</label>
+          <input id='text' name='text' 
+              onChange={onUpdateFieldChange} 
+              value={updateFields.text}/>
+          <label>Change Emoji</label>
+          <input id='text' name='text'
+              onChange = {onUpdateFieldChange}
+              value = {updateFields.emoji}/>
+        </form>
       </div>
     </div>
   )
