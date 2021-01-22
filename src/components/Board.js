de23f813a9565dc5c058656d42f6158dfd39de41
Alice D <alicedilanchian@gmail.com>
2021-01-22 16:55:26 -0500

    fixed function and moved it down to board instead of app

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
@@ -13,26 +12,41 @@ const Board = (props) => {
   const [cardList, setCardList] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
   
-  useEffect(() => {
-      axios.get(API_URL_BASE)
+  const getCards = useCallback(() => {
+    
+    axios.get(API_URL_BASE)
+    .then((response) => {
+        console.log(response)
+
+        //convert from array of card data to array of individual fields
+        const result = response.data.map( (arrayElement) => {
+          return arrayElement.card
+        })
+
+        setCardList(result)
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+    });
+  },[API_URL_BASE])
+
+  const deleteFunction = (cardID) => {
+    axios.delete(`https://inspiration-board.herokuapp.com/cards/${cardID}`)
         .then((response) => {
-            console.log(response)
-
-            //convert from array of card data to array of individual fields
-            const result = response.data.map( (arrayElement) => {
-              return arrayElement.card
-            })
-
-            setCardList(result)
+        
         })
         .catch((error) => {
           setErrorMessage(error.message);
         });
-    }, []);  
+  }
+
+  useEffect(() => {
+     getCards()
+    }, [getCards]);
 
   const boardCards = cardList.map((card) => {
     return (
-        <Card text={card.text} key={card.id} emoji={card.emoji}/>
+        <Card text={card.text} key={card.id} emoji={card.emoji} deleteFunction={deleteFunction} id={card.id}/>
     )
   })
 
@@ -47,3 +61,20 @@ Board.propTypes = {
 };
 
 export default Board;
+
+// function in app should receive card id
+// and use that id to issue a request to delete the card
+// reload the cards
+// pass down a function to the card.js to tell card which card to delete
+
+// a button on each card
+// passed down the function to card (using props),
+// that each card can use when clicking on that button
+
+
+// for the create a new card
+// similar to users submisison form in exquisite react
+// collect text
+// collect emoji as a form
+// use that to issue a post request
+// reload the cards
